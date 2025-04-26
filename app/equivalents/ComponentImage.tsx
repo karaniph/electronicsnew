ddd003a493a0663854cee8b350b80e62cb06859e
karaniph <kunkarani@gmail.com>
2025-04-26 07:53:03 +0300

    fix: render SVG circuit images using <img> for compatibility

--- app/equivalents/ComponentImage.tsx
+++ app/equivalents/ComponentImage.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
 export default function ComponentImage({ src, alt }: { src?: string; alt: string }) {
+  if (src && src.endsWith(".svg")) {
+    // Render SVGs with a regular img tag
+    return (
+      <div className="flex items-center justify-center w-16 h-16 bg-gray-100 rounded border">
+        <img src={src} alt={alt} width={48} height={48} className="object-contain" />
+      </div>
+    );
+  }
   return (
     <div className="flex items-center justify-center w-16 h-16 bg-gray-100 rounded border">
       <Image
